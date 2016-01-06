c6aedb4d2b68644543edafc8768351267276e336
bryan costanich <bryancostanich@hotmail.com>
2016-01-05 22:52:55 -0800

    lots more tests. :D

--- Source/FountainSharp/TestProjects/FountainSharp.Parse.Tests/FountainTests.fs
+++ Source/FountainSharp/TestProjects/FountainSharp.Parse.Tests/FountainTests.fs
@@ -15,95 +15,207 @@ let properNewLines (text: string) = text.Replace("\r\n", System.Environment.NewL
 //===== Scene Headings
 [<Test>]
 let ``Basic Scene Heading`` () =
-   let doc = """EXT. BRICK'S PATIO - DAY""" |> Fountain.Parse
+   let doc = "EXT. BRICK'S PATIO - DAY" |> Fountain.Parse
    doc.Blocks
-   |> should equal [SceneHeading [Literal """EXT. BRICK'S PATIO - DAY"""]]
+   |> should equal [SceneHeading [Literal "EXT. BRICK'S PATIO - DAY"]]
 
 [<Test>]
 let ``Forced (".") Scene Heading`` () =
-   let doc = """.BINOCULARS A FORCED SCENE HEADING - LATER""" |> Fountain.Parse
+   let doc = ".BINOCULARS A FORCED SCENE HEADING - LATER" |> Fountain.Parse
    doc.Blocks
-   |> should equal [SceneHeading [Literal """BINOCULARS A FORCED SCENE HEADING - LATER"""]]
+   |> should equal [SceneHeading [Literal "BINOCULARS A FORCED SCENE HEADING - LATER"]]
 
 [<Test>]
 let ``Lowercase known scene heading`` () =
-   let doc = """ext. brick's pool - day""" |> Fountain.Parse
+   let doc = "ext. brick's pool - day" |> Fountain.Parse
    doc.Blocks
-   |> should equal [SceneHeading [Literal """ext. brick's pool - day"""]]
+   |> should equal [SceneHeading [Literal "ext. brick's pool - day"]]
 
 [<Test>]
 let ``Known INT Scene Head`` () =
-   let doc = """INT DOGHOUSE - DAY""" |> Fountain.Parse
+   let doc = "INT DOGHOUSE - DAY" |> Fountain.Parse
    doc.Blocks
-   |> should equal [SceneHeading [Literal """INT DOGHOUSE - DAY"""]]
+   |> should equal [SceneHeading [Literal "INT DOGHOUSE - DAY"]]
 
 [<Test>]
 let ``Known EXT Scene Head`` () =
-   let doc = """EXT DOGHOUSE - DAY""" |> Fountain.Parse
+   let doc = "EXT DOGHOUSE - DAY" |> Fountain.Parse
    doc.Blocks
-   |> should equal [SceneHeading [Literal """EXT DOGHOUSE - DAY"""]]
+   |> should equal [SceneHeading [Literal "EXT DOGHOUSE - DAY"]]
 
 [<Test>]
 let ``Known EST Scene Head`` () =
-   let doc = """EST DOGHOUSE - DAY""" |> Fountain.Parse
+   let doc = "EST DOGHOUSE - DAY" |> Fountain.Parse
    doc.Blocks
-   |> should equal [SceneHeading [Literal """EST DOGHOUSE - DAY"""]]
+   |> should equal [SceneHeading [Literal "EST DOGHOUSE - DAY"]]
 
 [<Test>]
 let ``Known INT./EXT Scene Head`` () =
-   let doc = """INT./EXT DOGHOUSE - DAY""" |> Fountain.Parse
+   let doc = "INT./EXT DOGHOUSE - DAY" |> Fountain.Parse
    doc.Blocks
-   |> should equal [SceneHeading [Literal """INT./EXT DOGHOUSE - DAY"""]]
+   |> should equal [SceneHeading [Literal "INT./EXT DOGHOUSE - DAY"]]
 
 [<Test>]
 let ``Known INT/EXT Scene Head`` () =
-   let doc = """INT/EXT DOGHOUSE - DAY""" |> Fountain.Parse
+   let doc = "INT/EXT DOGHOUSE - DAY" |> Fountain.Parse
    doc.Blocks
-   |> should equal [SceneHeading [Literal """INT/EXT DOGHOUSE - DAY"""]]
+   |> should equal [SceneHeading [Literal "INT/EXT DOGHOUSE - DAY"]]
 
 [<Test>]
 let ``Known I/E Scene Head`` () =
-   let doc = """I/E DOGHOUSE - DAY""" |> Fountain.Parse
+   let doc = "I/E DOGHOUSE - DAY" |> Fountain.Parse
    doc.Blocks
-   |> should equal [SceneHeading [Literal """I/E DOGHOUSE - DAY"""]]
+   |> should equal [SceneHeading [Literal "I/E DOGHOUSE - DAY"]]
 
 //===== Synopses
 
 [<Test>]
 let ``Basic Synopses`` () =
-   let doc = """= Here is a synopses of this fascinating scene.""" |> Fountain.Parse
+   let doc = "= Here is a synopses of this fascinating scene." |> Fountain.Parse
    doc.Blocks
-   |> should equal [Synopses [Literal """Here is a synopses of this fascinating scene."""]]
+   |> should equal [Synopses [Literal "Here is a synopses of this fascinating scene."]]
 
 
 //===== Character
 
 [<Test>]
 let ``Character - Normal`` () =
-   let doc = """LINDSEY""" |> Fountain.Parse
+   let doc = "LINDSEY" |> Fountain.Parse
    doc.Blocks
-   |> should equal [Character [Literal """LINDSEY"""]]
+   |> should equal [Character [Literal "LINDSEY"]]
+
+[<Test>]
+let ``Character - With parenthetical extension`` () =
+   let doc = "LINDSEY (on the radio)" |> Fountain.Parse
+   doc.Blocks
+   |> should equal [Character [Literal "LINDSEY (on the radio)"]]
 
 [<Test>]
 let ``Character - With whitespace`` () =
-   let doc = """THIS IS ALL UPPERCASE BUT HAS WHITESPACE""" |> Fountain.Parse
+   let doc = "THIS IS ALL UPPERCASE BUT HAS WHITESPACE" |> Fountain.Parse
    doc.Blocks
-   |> should equal [Character [Literal """THIS IS ALL UPPERCASE BUT HAS WHITESPACE"""]]
+   |> should equal [Character [Literal "THIS IS ALL UPPERCASE BUT HAS WHITESPACE"]]
 
 [<Test>]
 let ``Character - With Numbers`` () =
-   let doc = """R2D2""" |> Fountain.Parse
+   let doc = "R2D2" |> Fountain.Parse
    doc.Blocks
-   |> should equal [Character [Literal """R2D2"""]]
+   |> should equal [Character [Literal "R2D2"]]
 
 [<Test>]
 let ``Character - Number first`` () =
-   let doc = """25D2""" |> Fountain.Parse
+   let doc = "25D2" |> Fountain.Parse
    doc.Blocks
-   |> should equal [Action [Literal """25D2"""]]
+   |> should equal [Action [Literal "25D2"]]
 
 [<Test>]
 let ``Character - Forced with at sign`` () =
-   let doc = """@McAvoy""" |> Fountain.Parse
+   let doc = "@McAvoy" |> Fountain.Parse
    doc.Blocks
-   |> should equal [Character [Literal """McAvoy"""]]
+   |> should equal [Character [Literal "McAvoy"]]
+
+[<Test>]
+let ``Character - with forced at and parenthetical extension`` () =
+   let doc = "@McAvoy (OS)" |> Fountain.Parse
+   doc.Blocks
+   |> should equal [Character [Literal "McAvoy (OS)"]]
+
+
+//===== Parenthetical
+
+[<Test>]
+let ``Parenthetical`` () =
+   let doc = "LINDSEY\r\n(quietly)" |> Fountain.Parse
+   doc.Blocks
+   |> should equal [Character [Literal "LINDSEY"]; Parenthetical [Literal "(quietly)"]];
+
+
+//===== Dialogue
+
+[<Test>]
+let ``Dialogue - Normal`` () =
+   let doc = "LINDSEY\r\nHello, friend." |> Fountain.Parse
+   doc.Blocks
+   |> should equal [Character [Literal "LINDSEY"]; Dialogue [Literal "Hello, friend."]]
+
+// WTF won't this compile?
+//[<Test>]
+//let ``Dialogue - After Parenthetical`` () =
+//   let doc = "LINDSEY\r\n(quietly)\r\nHello, friend." |> Fountain.Parse
+//   doc.Blocks
+//   |> should equal [Character [Literal "LINDSEY"]; Parenthetical [Literal "(quietly)"]; Dialogue [Literal "Hello, friend."]]
+
+
+//===== Page Break
+
+[<Test>]
+let ``PageBreak - ===`` () =
+   let doc = "===" |> Fountain.Parse
+   doc.Blocks
+   |> should equal [PageBreak]
+
+[<Test>]
+// TODO: should this be a synopses? probably, yeah? need clarification from the spec
+let ``PageBreak - == (not enough =)`` () =
+   let doc = "==" |> Fountain.Parse
+   doc.Blocks
+   |> should equal [Synopses [Literal "="]]
+
+[<Test>]
+let ``PageBreak - ==========`` () =
+   let doc = "==========" |> Fountain.Parse
+   doc.Blocks
+   |> should equal [PageBreak]
+
+[<Test>]
+let ``PageBreak - ======= (with space at end)`` () =
+   let doc = "======= " |> Fountain.Parse
+   doc.Blocks
+   |> should equal [PageBreak]
+
+
+[<Test>]
+let ``PageBreak - ======= blah (fail with other chars)`` () =
+   let doc = "======= blah" |> Fountain.Parse
+   doc.Blocks
+   |> should equal [Synopses [Literal "====== blah"]]
+
+
+//===== Lyrics
+
+[<Test>]
+let ``Lyric - normal`` () =
+   let doc = "~Birdy hop, he do. He hop a long." |> Fountain.Parse
+   doc.Blocks
+   |> should equal [Lyric [Literal "Birdy hop, he do. He hop a long."]]
+
+
+//===== Transition
+
+[<Test>]
+let ``Transition - normal`` () =
+   let doc = "CUT TO:" |> Fountain.Parse
+   doc.Blocks
+   |> should equal [Transition [Literal "CUT TO:"]]
+
+[<Test>]
+let ``Transition - forced`` () =
+   let doc = "> Burn to White." |> Fountain.Parse
+   doc.Blocks
+   |> should equal [Transition [Literal "Burn to White."]]
+
+//===== Centered
+
+[<Test>]
+let ``Centered`` () =
+   let doc = ">The End<" |> Fountain.Parse
+   doc.Blocks
+   |> should equal [Centered [Literal "The End"]]
+
+//// TODO: wtf doesn't this compile either?
+//[<Test>]
+//let ``Centered - with spaces`` () =
+//   let doc = "> The End <" |> Fountain.Parse
+//   doc.Blocks
+//   |> should equal [Centered [Literal "The End"]]
+
