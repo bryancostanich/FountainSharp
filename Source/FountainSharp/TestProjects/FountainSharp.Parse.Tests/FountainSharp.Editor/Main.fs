1dada222e992eea81742ad1e4d4e277e29784def
bryan costanich <bryancostanich@hotmail.com>
2016-01-06 18:34:17 -0800

    initial editor app check in.

---
+++ Source/FountainSharp/TestProjects/FountainSharp.Parse.Tests/FountainSharp.Editor/Main.fs
@@ -0,0 +1,10 @@
+﻿namespace FountainSharp.Editor
+open System
+open AppKit
+
+module main =
+    [<EntryPoint>]
+    let main args =
+        NSApplication.Init ()
+        NSApplication.Main (args)
+        0
---
+++ Source/FountainSharp/TestProjects/FountainSharp.Parse.Tests/FountainSharp.Editor/MainWindow.fs
@@ -0,0 +1,18 @@
+﻿namespace FountainSharp.Editor
+
+open System
+open Foundation
+open AppKit
+
+[<Register ("MainWindow")>]
+type MainWindow =
+    inherit NSWindow
+
+    new () = { inherit NSWindow () }
+    new (handle : IntPtr) = { inherit NSWindow (handle) }
+
+    [<Export ("initWithCoder:")>]
+    new (coder : NSCoder) = { inherit NSWindow (coder) }
+
+    override x.AwakeFromNib () =
+        base.AwakeFromNib ()
