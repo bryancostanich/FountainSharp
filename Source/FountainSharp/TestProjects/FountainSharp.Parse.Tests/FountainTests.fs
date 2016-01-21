513027b5878fa272cd4b91e4cc3560401086a9e6
bryan costanich <bryancostanich@hotmail.com>
2016-01-20 19:31:50 -0800

    fixed up tests to be compatible with forced properties.

--- Source/FountainSharp/TestProjects/FountainSharp.Parse.Tests/FountainTests.fs
+++ Source/FountainSharp/TestProjects/FountainSharp.Parse.Tests/FountainTests.fs
@@ -73,28 +73,28 @@ let ``Known I/E Scene Head`` () =
 let ``Scene Heading with line breaks and action`` () =
    let doc = "EXT. BRICK'S PATIO - DAY\r\n\r\nSome Action" |> Fountain.Parse
    doc.Blocks
-   |> should equal  [SceneHeading [Literal "EXT. BRICK'S PATIO - DAY"]; Action [HardLineBreak; Literal "Some Action"]]
+   |> should equal  [SceneHeading [Literal "EXT. BRICK'S PATIO - DAY"]; Action (false, [HardLineBreak; Literal "Some Action"])]
 
 
 [<Test>]
 let ``Scene Heading with more line breaks and action`` () =
    let doc = "EXT. BRICK'S PATIO - DAY\r\n\r\n\r\nSome Action" |> Fountain.Parse
    doc.Blocks
-   |> should equal  [SceneHeading [Literal "EXT. BRICK'S PATIO - DAY"]; Action [HardLineBreak; HardLineBreak; Literal "Some Action"]]
+   |> should equal  [SceneHeading [Literal "EXT. BRICK'S PATIO - DAY"]; Action (false, [HardLineBreak; HardLineBreak; Literal "Some Action"])]
 
 //===== Action
 [<Test>]
 let ``Action with line breaks`` () =
    let doc = "EXT. BRICK'S PATIO - DAY\r\n\r\nSome Action\r\n\r\nSome More Action" |> Fountain.Parse
    doc.Blocks
-   |> should equal  [SceneHeading [Literal "EXT. BRICK'S PATIO - DAY"]; Action [HardLineBreak; Literal "Some Action"; HardLineBreak; HardLineBreak; Literal "Some More Action"]]
+   |> should equal  [SceneHeading [Literal "EXT. BRICK'S PATIO - DAY"]; Action (false, [HardLineBreak; Literal "Some Action"; HardLineBreak; HardLineBreak; Literal "Some More Action"])]
 
 [<Test>]
 let ``Action with line breaks and no heading`` () =
    let doc = "Natalie looks around at the group, TIM, ROGER, NATE, and VEEK.\n
 TIM, is smiling broadly." |> Fountain.Parse
    doc.Blocks
-   |> should equal [Action [Literal "Natalie looks around at the group, TIM, ROGER, NATE, and VEEK."; HardLineBreak; HardLineBreak; Literal "TIM, is smiling broadly."]]
+   |> should equal [Action (false, [Literal "Natalie looks around at the group, TIM, ROGER, NATE, and VEEK."; HardLineBreak; HardLineBreak; Literal "TIM, is smiling broadly."])]
 
 
 
@@ -114,43 +114,43 @@ let ``Basic Synopses`` () =
 let ``Character - Normal`` () =
    let doc = "LINDSEY" |> Fountain.Parse
    doc.Blocks
-   |> should equal [Character [Literal "LINDSEY"]]
+   |> should equal [Character (false, [Literal "LINDSEY"])]
 
 [<Test>]
 let ``Character - With parenthetical extension`` () =
    let doc = "LINDSEY (on the radio)" |> Fountain.Parse
    doc.Blocks
-   |> should equal [Character [Literal "LINDSEY (on the radio)"]]
+   |> should equal [Character (false, [Literal "LINDSEY (on the radio)"])]
 
 [<Test>]
 let ``Character - With whitespace`` () =
    let doc = "THIS IS ALL UPPERCASE BUT HAS WHITESPACE" |> Fountain.Parse
    doc.Blocks
-   |> should equal [Character [Literal "THIS IS ALL UPPERCASE BUT HAS WHITESPACE"]]
-
+   |> should equal [Character (false, [Literal "THIS IS ALL UPPERCASE BUT HAS WHITESPACE"])]
+    
 [<Test>]
 let ``Character - With Numbers`` () =
    let doc = "R2D2" |> Fountain.Parse
    doc.Blocks
-   |> should equal [Character [Literal "R2D2"]]
+   |> should equal [Character (false, [Literal "R2D2"])]
 
 [<Test>]
 let ``Character - Number first`` () =
    let doc = "25D2" |> Fountain.Parse
    doc.Blocks
-   |> should equal [Action [Literal "25D2"]]
+   |> should equal [Action (false, [Literal "25D2"])]
 
 [<Test>]
 let ``Character - Forced with at sign`` () =
    let doc = "@McAvoy" |> Fountain.Parse
    doc.Blocks
-   |> should equal [Character [Literal "McAvoy"]]
+   |> should equal [Character (true, [Literal "McAvoy"])]
 
 [<Test>]
 let ``Character - with forced at and parenthetical extension`` () =
    let doc = "@McAvoy (OS)" |> Fountain.Parse
    doc.Blocks
-   |> should equal [Character [Literal "McAvoy (OS)"]]
+   |> should equal [Character (true, [Literal "McAvoy (OS)"])]
 
 
 //===== Parenthetical
@@ -159,13 +159,13 @@ let ``Character - with forced at and parenthetical extension`` () =
 let ``Parenthetical `` () =
    let doc = "LINDSEY\r\n(quietly)" |> Fountain.Parse
    doc.Blocks
-   |> should equal [Character [Literal "LINDSEY"]; Parenthetical [Literal "quietly"]];
+   |> should equal [Character (false, [Literal "LINDSEY"]); Parenthetical [Literal "quietly"]];
 
 [<Test>]
 let ``Parenthetical - After Dialogue`` () =
    let doc = "LINDSEY\r\n(quietly)\r\nHello, friend.\r\n(loudly)\r\nFriendo!" |> Fountain.Parse
    doc.Blocks
-   |> should equal [Character [Literal "LINDSEY"]; Parenthetical [Literal "quietly"]; Dialogue [Literal "Hello, friend."]; Parenthetical [Literal "loudly"]; Dialogue [Literal "Friendo!"]];
+   |> should equal [Character (false, [Literal "LINDSEY"]); Parenthetical [Literal "quietly"]; Dialogue [Literal "Hello, friend."]; Parenthetical [Literal "loudly"]; Dialogue [Literal "Friendo!"]];
 
 
 //===== Dialogue
@@ -174,13 +174,13 @@ let ``Parenthetical - After Dialogue`` () =
 let ``Dialogue - Normal`` () =
    let doc = "LINDSEY\r\nHello, friend." |> Fountain.Parse
    doc.Blocks
-   |> should equal [Character [Literal "LINDSEY"]; Dialogue [Literal "Hello, friend."]]
+   |> should equal [Character (false, [Literal "LINDSEY"]); Dialogue [Literal "Hello, friend."]]
 
 [<Test>]
 let ``Dialogue - After Parenthetical`` () =
    let doc = "LINDSEY\r\n(quietly)\r\nHello, friend." |> Fountain.Parse
    doc.Blocks
-   |> should equal [Character [Literal "LINDSEY"]; Parenthetical [Literal "quietly"]; Dialogue [Literal "Hello, friend."]]
+   |> should equal [Character (false, [Literal "LINDSEY"]); Parenthetical [Literal "quietly"]; Dialogue [Literal "Hello, friend."]]
 
 
 //===== Page Break
@@ -233,13 +233,13 @@ let ``Lyric - normal`` () =
 let ``Transition - normal`` () =
    let doc = "CUT TO:" |> Fountain.Parse
    doc.Blocks
-   |> should equal [Transition [Literal "CUT TO:"]]
+   |> should equal [Transition (false, [Literal "CUT TO:"])]
 
 [<Test>]
 let ``Transition - forced`` () =
    let doc = "> Burn to White." |> Fountain.Parse
    doc.Blocks
-   |> should equal [Transition [Literal "Burn to White."]]
+   |> should equal [Transition (true, [Literal "Burn to White."])]
 
 //===== Centered
 
@@ -262,9 +262,9 @@ let ``Centered - with spaces`` () =
 let ``Line Breaks`` () =
    let doc = "Murtaugh, springing...\n\nAn explosion of sound...\nAs it rises like an avenging angel ...\nHovers, shattering the air \n\nScreaming, chaos, frenzy.\nThree words that apply to this scene." |> Fountain.Parse
    doc.Blocks
-   |> should equal [Action [Literal "Murtaugh, springing..."; HardLineBreak; HardLineBreak; Literal "An explosion of sound..."; HardLineBreak; 
+   |> should equal [Action (false, [Literal "Murtaugh, springing..."; HardLineBreak; HardLineBreak; Literal "An explosion of sound..."; HardLineBreak; 
       Literal "As it rises like an avenging angel ..."; HardLineBreak;Literal "Hovers, shattering the air"; HardLineBreak; HardLineBreak;
-      Literal "Screaming, chaos, frenzy."; HardLineBreak; Literal "Three words that apply to this scene."]]
+      Literal "Screaming, chaos, frenzy."; HardLineBreak; Literal "Three words that apply to this scene."])]
   
 
 
@@ -274,7 +274,7 @@ let ``Notes - Inline`` () =
    let doc = "Some text and then a [[bit a of a note]]. And some more text." |> Fountain.Parse
    doc.Blocks
    // TODO: figure out the right output here. 
-   |> should equal [Action [Literal "fails anyway, it drops the note right now."]]
+   |> should equal [Action (false, [Literal "fails anyway, it drops the note right now."])]
    //|> should equal [Action [Literal "Some text and then a "];[Note [Literal"bit of a note"]]; Literal ". And some more text."]
 
 [<Test>]
@@ -294,51 +294,51 @@ let ``Notes - Block`` () =
 let ``Emphasis - Bold`` () =
    let doc = "**This is bold Text**" |> Fountain.Parse
    doc.Blocks
-   |> should equal [Action [Strong [Literal "This is bold Text"]]]
+   |> should equal [Action (false, [Strong [Literal "This is bold Text"]])]
 
 [<Test>]
 let ``Emphasis - Italics`` () =
    let doc = "*This is italic Text*" |> Fountain.Parse
    doc.Blocks
-   |> should equal [Action [Italic [Literal "This is italic Text"]]]
+   |> should equal [Action (false, [Italic [Literal "This is italic Text"]])]
 
 [<Test>]
 let ``Emphasis - Bold Italic`` () =
    let doc = "***This is bold Text***" |> Fountain.Parse
    doc.Blocks
-   |> should equal [Action [Strong [Italic [Literal "This is bold Text"]]]]
+   |> should equal [Action (false, [Strong [Italic [Literal "This is bold Text"]]])]
 
 //TODO: i don't even know how to write this test. it fails anyway. (look at next one, for clues)
 [<Test>]
 let ``Emphasis - Nested Bold Italic`` () =
  let doc = "**This is bold *and Italic Text***" |> Fountain.Parse
  doc.Blocks
-   |> should equal [Action [Literal "need to figure out how to write the value here."]]
+   |> should equal [Action (false, [Literal "need to figure out how to write the value here."])]
 // |> should equal [Action [Strong [Literal "This is bold "] [Italic [Literal "and Italic Text"]]]]
 
 [<Test>]
 let ``Emphasis - Nested Italic Bold`` () =
    let doc = "From what seems like only INCHES AWAY.  _Steel's face FILLS the *Leupold Mark 4* scope_." |> Fountain.Parse
    doc.Blocks
-   |> should equal [Action [Literal "From what seems like only INCHES AWAY.  "; Underline [Literal "Steel's face FILLS the "; Italic [Literal "Leupold Mark 4"]; Literal " scope"]; Literal "."]]
+   |> should equal [Action (false, [Literal "From what seems like only INCHES AWAY.  "; Underline [Literal "Steel's face FILLS the "; Italic [Literal "Leupold Mark 4"]; Literal " scope"]; Literal "."])]
 
 [<Test>]
 let ``Emphasis - with escapes`` () =
    let doc = "Steel enters the code on the keypad: **\*9765\***" |> Fountain.Parse
    doc.Blocks
-   |> should equal [Action [Literal "Steel enters the code on the keypad: "; Strong [Literal "*9765*"]]]
+   |> should equal [Action (false, [Literal "Steel enters the code on the keypad: "; Strong [Literal "*9765*"]])]
 
 [<Test>]
 let ``Emphasis - italics with spaces to left`` () =
    let doc = "He dialed *69 and then *23, and then hung up." |> Fountain.Parse
    doc.Blocks
-   |> should equal [Action [Literal "He dialed "; Italic [Literal "69 and then "]; Literal "23, and then hung up."]]
+   |> should equal [Action (false, [Literal "He dialed "; Italic [Literal "69 and then "]; Literal "23, and then hung up."])]
 
 [<Test>]
 let ``Emphasis - italics with spaces to left but escaped`` () =
    let doc = "He dialed *69 and then 23\*, and then hung up.." |> Fountain.Parse
    doc.Blocks
-   |> should equal [Action [Literal "He dialed *69 and then 23*, and then hung up.."]]
+   |> should equal [Action (false, [Literal "He dialed *69 and then 23*, and then hung up.."])]
 
 // TODO: are line breaks being recognized properly here? i think not. i think i need more line break cases
 [<Test>]
@@ -347,7 +347,7 @@ let ``Emphasis - between line breaks`` () =
              \r\n\
              This is going to be BAD.*" |> Fountain.Parse
    doc.Blocks
-   |> should equal [Action [Literal @"As he rattles off the long list, Brick and Steel *share a look.\r\n\"]; Action [Literal @"\r\n\"]; Action [Literal "This is going to be BAD.*"]]
+   |> should equal [Action (false, [Literal @"As he rattles off the long list, Brick and Steel *share a look."; HardLineBreak; HardLineBreak; Literal "This is going to be BAD.*"])]
 
 //===== Indenting
 // TODO
